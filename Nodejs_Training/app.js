e94ec0b04d9b33458c691057b04a78bae3890a78
AKASH A MENON <46593739+akashamenon@users.noreply.github.com>
2023-02-08 16:03:49 +0530

    Add files via upload

---
+++ Nodejs_Training/app.js
@@ -0,0 +1,7 @@
+const { createServer } = require("http");
+const server = createServer((request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write("<h1>Hello World!</h1>");
+  return response.end();
+});
+server.listen(8080);
